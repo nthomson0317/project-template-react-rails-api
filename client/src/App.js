67259b0a12b60c58a17f6caea728d34da2e624a2
Muhammad Zahid <muhammadali775@gmail.com>
2021-07-08 17:13:58 -0400

    Login function part 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,117 @@
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Link, Route, withRouter} from 'react-router-dom'
+import NavBar from './Components/NavBar';
+import User from './Components/User';
+import Login from './Components/Login';
+import Home from './Components/Home';
+import {useState} from 'react';
+
 
 function App() {
+  const [currentUser,setCurrentUser]=useState({
+    id: 0,
+    username:'',
+    name:"",
+    rating: 0,
+    age:0,
+    profile_pic:"",
+    country:""
+    })
+ 
+
+
+
+  const handleSubmit = (formData) => {
+     
+      console.log(formData)
+
+      fetch("http://localhost:3000/login", {
+          method: "POST",
+          headers: {
+              "Content-type": "application/json"
+          },
+          body: JSON.stringify(
+              formData
+          )
+          })
+          .then(res => res.json())
+          .then(handleResponse)
+  }
+
+
+  let handleResponse= (res) => {
+    setCurrentUser=res   
+  }
+
+
+console.log(currentUser)
   return (
-    <div></div>
+    
+           <div>
+              <NavBar
+                
+                //  loginState={loginState}
+                //  toggleLoginState={toggleLoginState}
+              />
+              <Switch>
+                
+
+               {/* FIRST ROUTE */}
+              <Route path={'/login'}
+                 render={routerProps => {
+                 return <div>
+                            <Login 
+                             {...routerProps}handleSubmit={handleSubmit}>
+                            </Login>
+                            
+                       </div>}}>
+              </Route>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        {/* <Route path={'/cart'}
+        render={routerProps => {
+        return <div>
+        <User
+          // {...routerProps}carts={carts}
+          // currentUser={currentUser}
+          // beanies={beanies}
+          // deleteFromCart={deleteFromCart}
+          // updateCartsState={updateCartsState}
+            >
+          </User>
+          </div>
+        }}>
+        </Route>
+       
+        <Route path={'/'}>
+          <Home />
+        </Route> */}
+      </Switch>
+    </div>
+
+
+
+
+
+      
+   
   );
 }
 
---
+++ client/src/Components/Login.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import {useState} from 'react';
+
+
+
+
+export default function Login(props) {
+   
+    // STATE BELOW
+    const [userName, setUserName]=useState("")
+     //STATE NUMBER 2
+    const [password, setPassword]=useState("")
+
+
+
+
+
+
+
+
+  
+let submitHandler = (e) => {
+    e.preventDefault()
+
+    let formData = { 
+        username: userName,    
+        password: password}      
+        props.handleSubmit(formData)
+  }
+
+
+const handleChangeUserName = (e) => {
+      setUserName(e.target.value)}
+
+const handleChangePassword = (e) => {
+      setPassword(e.target.value)}
+
+
+
+
+
+
+ 
+    return (
+        <div>    
+            <form onSubmit={submitHandler}>
+             <h1>{props.formName}</h1>
+             <label htmlFor="username">Username:</label>
+                <input type="text" autoComplete="off"
+                 name="username"
+                 value={userName}
+                 onChange={handleChangeUserName}
+                />
+             <label htmlFor="password">Password:</label>
+             <input type="password" autoComplete="off"
+                name="password"
+                value={password}
+                onChange={handleChangePassword}
+             />
+               <input type="submit" value="Submit"/>
+             </form>
+        </div>
+    )
+}
